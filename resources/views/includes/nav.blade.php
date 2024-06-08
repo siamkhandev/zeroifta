5e2320796a413c42ae74f57e49563787d24e84ba
Gulraiz Azam <gulraiz@mail.redsignal.net>
2024-06-08 16:50:26 +0500

    resolved

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -3,9 +3,55 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+            @if(Request::is('/'))
             <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
           </ol>
           <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+          @elseif(Request::is('profile'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Profile</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Profile</h6>
+          @elseif(Request::is('companies'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Companies</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Companies</h6>
+          @elseif(Request::is('plans'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Plans</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Plans</h6>
+          @elseif(Request::is('payments'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Payments</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Payments</h6>
+          @elseif(Request::is('fuel_taxes'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Fuel Taxes</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Fuel Taxes</h6>
+          @elseif(Request::is('contactus/all'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Contact Us</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Contact Us</h6>
+          @elseif(Request::is('drivers'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Drivers</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Drivers</h6>
+          @elseif(Request::is('vehicles/all'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Vehicles</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Vehicles</h6>
+          @elseif(Request::is('driver/vehicles'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Driver Vehicles</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Driver Vehicles</h6>
+          @elseif(Request::is('subscribe'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Subscribe</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Subscribe</h6>
+          @elseif(Request::is('contactus'))
+          <li class="breadcrumb-item text-sm text-white active" aria-current="page">Contact us</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Contact us</h6>
+          @endif
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
