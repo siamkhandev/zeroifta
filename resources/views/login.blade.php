a41a954afbdfbe181d5c6040d542ddf79c180d97
Gulraiz Azam <gulraiz@mail.redsignal.net>
2024-08-29 23:12:37 +0500

    resolved

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -85,8 +85,8 @@
               <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('{{asset('images/sidebar.jpg')}}');
           background-size: cover;">
                 <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Endless roads and endless resolve – that’s the spirit of trucking."</h4>
+                <p class="text-white position-relative">Trucking is not a job; it’s a way of life. It’s about more than just hauling freight; it’s about dedication, pride, and a love for the open road.</p>
               </div>
             </div>
           </div>
