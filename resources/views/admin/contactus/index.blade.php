ee5cfa2d3882aa683e5a7bc93ecd0723df6911ea
muzammalrahim <creative.joomdev@gmail.com>
2024-11-16 02:44:19 +0500

    css chnage

--- resources/views/admin/contactus/index.blade.php
+++ resources/views/admin/contactus/index.blade.php
@@ -95,7 +95,11 @@
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="closeBtn" data-bs-dismiss="modal" aria-label="Close">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 28 28" fill="none">
+                        <path d="M14 10.8894L24.8894 0L28 3.11062L17.1106 14L28 24.8894L24.8894 28L14 17.1106L3.11062 28L0 24.8894L10.8894 14L0 3.11062L3.11062 0L14 10.8894Z" />
+                      </svg>
+                    </button>
                   </div>
                   <div class="modal-body">
                     <div class="text-center">
