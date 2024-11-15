3d39fb378d964a26f3fdb04458828a6b0233f921
Gulraiz azam <gulraizazam0@gmail.com>
2024-11-15 12:36:24 +0500

    resolved

--- app/Http/Controllers/Company/DriversController.php
+++ app/Http/Controllers/Company/DriversController.php
@@ -105,7 +105,7 @@ class DriversController extends Controller
     public function track($id)
     {
         $userName = User::find($id)->name;
-        $trip = Trip::where('user_id', $id)->first();
+        $trip = Trip::where('user_id', $id)->latest()->first();
         return view('company.drivers.track',get_defined_vars());
     }
     public function importForm()
