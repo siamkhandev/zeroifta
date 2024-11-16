f5e34691d5975e0d7459b69a972e58c6745194e4
gulraizazam <gulraizazam0@gmail.com>
2024-11-16 20:55:43 +0500

    resolved

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -42,7 +42,7 @@ class TripController extends Controller
             $lat = number_format((float) $station['latitude'], 4);
             $lng = number_format((float) $station['longitude'], 4);
 
-            if (isset($ftpData[$lat][$lng])) {
+            //if (isset($ftpData[$lat][$lng])) {
                 $price = $ftpData[$lat][$lng]['price'] ?? 0.00;
 
                 // Save the valid gas station
@@ -55,7 +55,7 @@ class TripController extends Controller
                     'price'     => $price,
                 ]);
                 $storedStations[] = $fuelStation;
-            }
+           // }
             
         }
        }
