2420de1b3414eccb5a5a07e6736e4c83ce2a6ef6
gulraizazam <gulraizazam0@gmail.com>
2024-11-17 22:32:41 +0500

    resolved

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/calculate-ifta', [IFTAController::class, 'findCheapestFuelStops']);
 //Route::post('/findgas', [IFTAController::class, 'findFuelStations']);
-Route::post('/findgas', [IFTAController::class, 'getDecodedPolyline']);
+Route::post('/findgas', [IFTAController::class, 'getDecodedPolyline'])->name('findgas');
 Route::post('login',[AuthController::class,'login']);
 Route::post('forgot-password', [AuthController::class, 'sendResetLinkEmail']);
 Route::middleware('auth:api')->group( function () {
