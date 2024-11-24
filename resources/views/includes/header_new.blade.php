357c4eabade07c3e2760ebdba4c78c839a2443fa
gulraizazam <gulraizazam0@gmail.com>
2024-11-24 20:48:02 +0500

    resolved

--- resources/views/includes/header_new.blade.php
+++ resources/views/includes/header_new.blade.php
@@ -95,7 +95,7 @@
                 fill="" />
         </svg>
     </div>
-  @else if(auth()->user()->theme == 'light')
+  @elseif(auth()->user()->theme == 'light')
     <div id="light-themeIcon" class="light-themeIcon hf-svg">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
             <path
