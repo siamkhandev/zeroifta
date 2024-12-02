ad6fefc4282975387f58e7d4fd9ba845afd979b9
Gulraiz azam <gulraizazam0@gmail.com>
2024-12-02 12:34:55 +0500

    resolved stops

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -343,7 +343,7 @@ class TripController extends Controller
             }
         }
         $stops = Tripstop::where('trip_id', $trip->id)->get();
-        $driverVehicle = DriverVehicle::where('user_id', $request->user_id)->first();
+        $driverVehicle = DriverVehicle::where('driver_id', $trip->user_id)->first();
         $vehicle = Vehicle::where('id', $driverVehicle->vehicle_id)->first();
         $vehicle->vehicle_image = 'http://zeroifta.alnairtech.com/vehicles/' . $vehicle->vehicle_image;
         if($trip){
