1e0ee5af1540669d955e2aae109bfa4378a0c374
gulraizazam <gulraizazam0@gmail.com>
2024-12-26 22:57:39 +0500

    Resolved

--- resources/views/company/driver_vehicles/create.blade.php
+++ resources/views/company/driver_vehicles/create.blade.php
@@ -62,9 +62,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="reassignModalLabel">Reassign Vehicle</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+               
             </div>
             <div class="modal-body">
                 <!-- Message will be populated dynamically -->
