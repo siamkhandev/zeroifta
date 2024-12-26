1842c16a7b9a25033ec9dacf7e4bc71b23f83175
Gulraiz azam <gulraizazam0@gmail.com>
2024-12-26 16:54:42 +0500

    Resolved

--- app/Http/Controllers/Company/DriverVehiclesController.php
+++ app/Http/Controllers/Company/DriverVehiclesController.php
@@ -21,7 +21,7 @@ class DriverVehiclesController extends Controller
     {
         $drivers = CompanyDriver::with('driver')->where('company_id',Auth::id())->get();
         $vehicles = Vehicle::where('company_id',Auth::id())->get();
-        $check = DriverVehicle::whereIn('vehicle_id',$vehicles)->get();
+        //$check = DriverVehicle::whereIn('vehicle_id',$vehicles)->get();
         //$vehicles = Vehicle::where('company_id',Auth::id())->whereNotIn('id',$check->pluck('vehicle_id'))->get();
         return view('company.driver_vehicles.create',get_defined_vars());
     }
