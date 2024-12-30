50628ad3622fc4ae5390e129974ab1ecb8d4f706
gulraizazam <gulraizazam0@gmail.com>
2024-12-30 21:34:33 +0500

    Resolved

--- resources/views/includes/sidebar_admin_new.blade.php
+++ resources/views/includes/sidebar_admin_new.blade.php
@@ -17,6 +17,7 @@
     <div class="sc-pad">
       <h4 class="head-14Med">{{ __('messages.Main Menu')}}</h4>
     </div>
+    
     <div class="pt-2">
       <ul class="sidebar-list">
         <li>
@@ -40,7 +41,7 @@
           </a>
         </li>
         <li>
-          <a href="{{route('profile')}}" class="sidebar_li {{ Request::is('profile') ? 'menu_active' : '' }}">
+          <a href="{{route('profile')}}" class="sidebar_li {{ Request::is(app()->getLocale() . '/profile') ? 'active' : '' }}">
             <span class="fill-svg">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path
