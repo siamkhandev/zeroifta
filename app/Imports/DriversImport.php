523a51ad6b7203b5c54eb5a25a199722cb101d54
Gulraiz azam <gulraizazam0@gmail.com>
2024-12-31 12:03:11 +0500

    Resolved

--- app/Imports/DriversImport.php
+++ app/Imports/DriversImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\CompanyDriver;
 use App\Models\User;
 use App\Models\Vehicle;
 use Illuminate\Support\Facades\Auth;
@@ -37,7 +38,7 @@ class DriversImport implements ToModel, WithHeadingRow,SkipsOnFailure
         $licenseStartDate = is_numeric($row['license_start_date'])
         ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['license_start_date'])->format('Y-m-d')
         : $row['license_start_date'];
-        return new User([
+        $drivers =  new User([
             'first_name' => $row['first_name'],
             'last_name' => $row['last_name'],
             'driver_id' => $row['driver_id'],
@@ -52,5 +53,10 @@ class DriversImport implements ToModel, WithHeadingRow,SkipsOnFailure
             'role' => 'driver',
             'company_id' => Auth::id(),
         ]);
+        CompanyDriver::create([
+            'company_id' => Auth::id(),
+            'driver_id' => $row['driver_id'],
+        ]);
+        return $drivers;
     }
 }
