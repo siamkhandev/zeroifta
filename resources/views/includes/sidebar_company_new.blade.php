1c53b1f4e8db305599ba5c8ad974b732566fd759
gulraizazam <gulraizazam0@gmail.com>
2025-01-01 20:35:57 +0500

    Resolved

--- resources/views/includes/sidebar_company_new.blade.php
+++ resources/views/includes/sidebar_company_new.blade.php
@@ -76,7 +76,7 @@
           </a>
         </li>
         <li>
-          <a href="{{route('allvehicles')}}" class="sidebar_li {{ Request::is(app()->getLocale() .'/vehicles/all*') ? 'menu_active' : '' }}">
+          <a href="{{route('allvehicles')}}" class="sidebar_li {{ Request::is(app()->getLocale() .'/vehicles*') ? 'menu_active' : '' }}">
             <span class="fill-svg">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path
