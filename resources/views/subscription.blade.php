c99b9533522bd81c77183ef5d23949539a34dc76
tomfoodery <foodplatform@outlook.com>
2025-01-02 15:15:19 +0500

    resolved

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -980,6 +980,7 @@
   </div>
 </body>
 <script src="{{asset('assets/js/script.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <script>
 
     $("#plansSwitchCheckbox").on('change', function() {
