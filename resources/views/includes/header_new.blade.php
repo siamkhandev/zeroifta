649cf344928b6885dcb5044cf7184c2985e4a12a
gulraizazam <gulraizazam0@gmail.com>
2025-01-02 21:22:48 +0500

    Resolved

--- resources/views/includes/header_new.blade.php
+++ resources/views/includes/header_new.blade.php
@@ -32,7 +32,7 @@
     @elseif($currentSegment2 == 'drivers' && $currentSegment3 == 'all')
         <span class="bc-text">{{__('messages.Pages / Drivers')}}</span>
         <h1 class="head-1">{{__('messages.Manage Drivers')}}</h1>
-        @elseif($currentSegment2 == 'contactus' && $currentSegment3 == 'all')
+        @elseif($currentSegment2 == 'company' && $currentSegment3 == 'contactus')
         <span class="bc-text">{{__('messages.Pages / Contact Forms')}}</span>
         <h1 class="head-1">{{__('messages.Contact Forms')}}</h1>
     @elseif($currentSegment2 == 'driver' && $currentSegment3 == 'vehicles')
