14f51c2f8bc77e3d498cd69c44733fb9709fd17f
gulraizazam <gulraizazam0@gmail.com>
2025-01-10 20:25:24 +0500

    Resolved

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -111,6 +111,7 @@ CpNLB7aULQtFKuJCSUZtdRs33b9s3e3lYJRUFOzOqswk9gCl5uu0
        
         // Parse decrypted data
         $cardDetails = json_decode($decryptedData, true);
+        Stripe::setApiKey(env('STRIPE_SECRET'));
         $token = Token::create([
             'card' => [
                 'number'    => $cardDetails['number'],
