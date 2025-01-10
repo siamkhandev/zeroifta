a0a7b59dc29e82375d11181be21dd2361b3f0f9c
gulraizazam <gulraizazam0@gmail.com>
2025-01-10 21:51:53 +0500

    Resolved

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -134,7 +134,7 @@ CpNLB7aULQtFKuJCSUZtdRs33b9s3e3lYJRUFOzOqswk9gCl5uu0
             }
             $stripe = new \Stripe\StripeClient('pk_test_AvPEuYEvHgZr9uN2f8KxzfGn00wLRXCSAb');
             $getMonth = explode('/',$cardDetails['expiry']);
-            dd($getMonth);
+          
            $token =  $stripe->tokens->create([
               'card' => [
                 'number' => $cardDetails['cardNumber'],
