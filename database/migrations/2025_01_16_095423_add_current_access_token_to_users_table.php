b0604d24b8be31d42dda996aae3d37ff299bf839
tomfoodery <foodplatform@outlook.com>
2025-01-16 15:00:03 +0500

    Resolved

--- database/migrations/2025_01_16_095423_add_current_access_token_to_users_table.php
+++ database/migrations/2025_01_16_095423_add_current_access_token_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('current_access_token')->nullable()->after('remember_token');
+            $table->text('current_access_token')->nullable()->after('remember_token');
         });
     }
 
