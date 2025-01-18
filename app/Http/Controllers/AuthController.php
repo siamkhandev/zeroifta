49e1594cafa9614e68535d8ed180b065e2e876ba
gulraizazam <gulraizazam0@gmail.com>
2025-01-18 19:31:40 +0500

    resolved inv

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
             $user->save();
             $user->token = $token;
             if($user->driver_image){
-                $user->image =url('/drivers/').$user->driver_image;
+                $user->image =url('/drivers/'.$user->driver_image);
             }else{
                 $user->image = null;
             }
@@ -106,7 +106,7 @@ class AuthController extends Controller
             return response()->json(['status'=>422,'message' => $validator->errors()->first(),'data'=>(object)[]], 422);
         }
         $user = User::whereId($request->user_id)->first();
-        $user->image = url('/images/').$user->image;
+        $user->image = url('/images/'.$user->image);
         return response()->json(['status'=>200,'message'=>'Profile Fetched successfully','data' => $user], 200);
     }
     public function getProfile(Request $request)
@@ -134,7 +134,7 @@ class AuthController extends Controller
                 $vehicle->vehicle_image = url('/vehicles/' . $vehicle->vehicle_image);
             }
             $user->vehicle = $vehicle;
-            $user->image = url('/images/').$user->image;
+            $user->image = url('/images/'.$user->image);
             $user->token=null;
             $features = [];
             $checkSubscription = Subscription::where('user_id',$user->id)->where('status','active')->first();
