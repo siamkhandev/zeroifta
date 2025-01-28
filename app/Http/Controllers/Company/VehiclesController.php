97148e4b4a6997acdc1116b85ddf3a1a159f3ef5
tomfoodery <foodplatform@outlook.com>
2025-01-28 13:00:32 +0500

    plans added

--- app/Http/Controllers/Company/VehiclesController.php
+++ app/Http/Controllers/Company/VehiclesController.php
@@ -32,7 +32,7 @@ class VehiclesController extends Controller
     {
 
         $data = $request->validate([
-            'vehicle_id'=>'required',
+            'vehicle_id'=>'required,unique:vehicles',
             "vin"=>'required',
             "year"=>'required',
             "truck_make"=>'required',
