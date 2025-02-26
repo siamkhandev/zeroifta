360f6ebd1d45c3039b65b7af8a4fdbb8047315dd
gulraizazam <gulraizazam0@gmail.com>
2025-02-26 22:19:50 +0500

    resolved

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -427,9 +427,10 @@ class TripController extends Controller
         }
 
         $response = Http::get($url);
-        dd($response);
+       
         if ($response->successful()) {
             $data = $response->json();
+            dd($data);
             if($data['routes'] && $data['routes'][0]){
                 $route = $data['routes'][0];
                 if (!empty($data['routes'][0]['legs'])) {
