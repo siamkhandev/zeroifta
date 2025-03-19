ac80a25dda13de9252de8c862dc5fd0edb1b1350
tomfoodery <foodplatform@outlook.com>
2025-03-19 13:08:03 +0500

    resolved

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -11,7 +11,12 @@ class Trip extends Model
     protected $fillable = [
         'user_id', 'start_lat', 'start_lng', 'end_lat', 'end_lng','status','vehicle_id','updated_start_lat','updated_start_lng','updated_end_lat','updated_end_lng','polyline','polyline_encoded','distance','duration','start_address','end_address','start_city','start_state','end_city','end_state'
     ];
-
+    protected $casts = [
+        'updated_start_lat' => 'string',
+        'updated_start_lng' => 'string',
+        'updated_end_lat' => 'string',
+        'updated_end_lng' => 'string',
+    ];
     public function driverVehicle()
     {
         return $this->hasOne(DriverVehicle::class, 'driver_id', 'user_id');
